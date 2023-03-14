7fc39108d0100583f3822093280a99a31fc1dbf8
thanhluann <imthanhluan@gmail.com>
2023-03-14 17:27:49 +0700

    fix delete postCategory problem

--- src/post/services/category.service.ts
+++ src/post/services/category.service.ts
@@ -58,7 +58,7 @@ export class CategoryService {
       )
     }
 
-    await this.categoryRepository.findOneAndDelete({ catetory: id })
+    await this.categoryRepository.findOneAndDelete({ _id: id })
     return { category_id: id }
   }
 
